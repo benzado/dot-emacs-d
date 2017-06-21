942bcddfcd297a1700a887a918f60b1c216fff76
Benjamin Ragheb <ben@benzado.com>
2017-06-20 23:44:20 -0400

    Macintosh: hexify path URIs, per documentation
    I haven't had any problems with the code, but I learned about the
    `url-hexify-string` function and figured I would tie up this loose
    end.

--- lisp/init-macintosh.el
+++ lisp/init-macintosh.el
@@ -3,8 +3,8 @@
 (setq locate-command "mdfind")
 
 ;; Update Terminal.app's title bar depending on the file being edited.
-;; In theory, the path should be URI-escaped, but that hasn't mattered
-;; so far. See /etc/bashrc_Apple_Terminal
+;; The path should be URI-escaped, hence `url-hexify-string`.
+;; See /etc/bashrc_Apple_Terminal for more.
 
 (defun my/update-mac-terminal-title-for-window (window &optional norecord)
   (unless window-system ; nil on text terminals
@@ -12,7 +12,7 @@
      (format "\e]6;%s\a"
 	     (let ((path (buffer-file-name (window-buffer window))))
 	       (if path
-		   (concat "file://" path)
+		   (concat "file://" (url-hexify-string path))
 		   ""))))))
 
 (advice-add 'select-window :after 'my/update-mac-terminal-title-for-window)
