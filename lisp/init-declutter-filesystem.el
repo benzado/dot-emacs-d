e600e833f293430aa950ca501162864680d3b315
Benjamin Ragheb <ben@benzado.com>
2017-06-25 21:11:32 -0400

    Simplify file system decluttering
    Functionally the same, but uses a let-expression instead of defining
    constants, to avoid polluting the environment.

--- lisp/init-declutter-filesystem.el
+++ lisp/init-declutter-filesystem.el
@@ -3,16 +3,12 @@
 ;; temporary directory. The auto-save-file-list (which stores the list
 ;; of open files) will be saved there, too.
 
-(defconst my-auto-save-directory
-  (expand-file-name "emacs-auto-saves/" temporary-file-directory))
-(make-directory my-auto-save-directory :parents)
-(setq auto-save-file-name-transforms `((".*" ,my-auto-save-directory t)))
-
-(setq auto-save-list-file-prefix
-      (expand-file-name "emacs-auto-save-list-" temporary-file-directory))
-
-(defconst my-backup-directory
-  (expand-file-name "emacs-backups" temporary-file-directory))
-(setq backup-directory-alist `(("." . ,my-backup-directory)))
+(let ((default-directory temporary-file-directory))
+  (let ((my-auto-save-directory (expand-file-name "emacs-auto-saves/")))
+    (make-directory my-auto-save-directory :parents)
+    (setq auto-save-file-name-transforms `((".*" ,my-auto-save-directory t))))
+  (setq auto-save-list-file-prefix (expand-file-name "emacs-auto-save-list-"))
+  (let ((my-backup-directory (expand-file-name "emacs-backups")))
+    (setq backup-directory-alist `(("." . ,my-backup-directory)))))
 
 (provide 'init-declutter-filesystem)
