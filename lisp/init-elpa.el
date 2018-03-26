b71d00f2c13e862423e52a3d93a3e4d963e903e9
Benjamin Ragheb <ben@benzado.com>
2018-03-25 20:11:08 -0400

    Use plain old HTTP for packages on Windows
    Windows "just doesn't" support HTTPS for talking to package archives,
    so this change sets the appropriate URL automatically.

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -1,13 +1,24 @@
 ;; -*- lexical-binding: t; -*-
 
-;; Use MELPA to manage packages
 ;; M-x package-list-packages to browse and install
 
 (require 'package)
 
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/")
-             'append)
+;; Declare package sources. HTTPS doesn't work on Windows.
+
+(let* ((no-https (and (eq system-type 'windows-nt)
+		      (not (gnutls-available-p))))
+       (proto (if no-https "http:" "https:")))
+  (setq package-archives
+	`(("gnu" . ,(concat proto "//elpa.gnu.org/packages/"))
+	  ("melpa" . ,(concat proto "//melpa.org/packages/")))))
+
+;; If a package exists in multiple repositories, the one with the
+;; highest priority is used, even if it is an older version.
+
+(setq package-archive-priorities
+      '(("gnu" . 1)
+	("melpa" . 0)))
 
 (setq package-enable-at-startup nil) ; don't autoload
 
