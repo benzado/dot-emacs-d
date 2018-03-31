a04a90e53107f7b92553c648f4e5598abdb9e64d
Benjamin Ragheb <ben@benzado.com>
2018-03-31 17:55:53 -0400

    init-org-mobile: the files are always encrypted
    Previously, encyption was conditional on the existence of the password
    file; that kind of made sense when there was only one machine, but now
    I am using Emacs on another machine, and the files ARE encrypted,
    whether I have the password file or not. So, I changed the
    configuration to reflect that, and also made the init/kill hooks
    conditional, because it doesn't make sense to "automatically"
    pull/push if it needs to prompt for a password.

--- lisp/init-org-mobile.el
+++ lisp/init-org-mobile.el
@@ -5,7 +5,12 @@
 
 ;; MobileOrg: Org Mode on the go!
 
+;; I'm currently using Dropbox to synchronize these files, and I'm
+;; slightly paranoid, so I encrypt them.
+
 (setq org-mobile-directory (expand-file-name "~/Dropbox/Apps/MobileOrg"))
+
+(setq org-mobile-use-encryption t)
 
 (setq org-mobile-inbox-for-pull (expand-file-name "inbox.org" org-directory))
 
@@ -20,22 +25,19 @@
                                                ; to problems down the
                                                ; road...
 
-;; If a file named .mobileorg-password exists in the org-directory, enable
-;; encryption and use the contents of that file as the password.
+;; If a file named .mobileorg-password exists in the org-directory,
+;; use the contents of that file as the password, and also
+;; automatically pull after init and push before quitting.
 
 (let ((password-file (expand-file-name ".mobileorg-password" org-directory)))
   (if (file-readable-p password-file)
       (progn
-        (setq org-mobile-use-encryption t)
         (setq org-mobile-encryption-password
               (with-temp-buffer
                 (insert-file-contents-literally password-file)
-                (buffer-substring-no-properties (point-min) (point-max)))))
+                (buffer-substring-no-properties (point-min) (point-max))))
+        (add-hook 'after-init-hook #'org-mobile-pull)
+        (add-hook 'kill-emacs-hook #'org-mobile-push))
     (message "Can't find MobileOrg password at %s" password-file)))
-
-;; Automatically pull after init and push before quit
-
-(add-hook 'after-init-hook #'org-mobile-pull)
-(add-hook 'kill-emacs-hook #'org-mobile-push)
 
 (provide 'init-org-mobile)
