312b33da03271a67975eb09225ca1653229c2f05
Benjamin Ragheb <ben@benzado.com>
2018-04-12 23:50:47 -0400

    improve ssh-agent.sh: ssh-add + cleanup
    - automatically run ssh-add when the agent is started, because that's
      the whole point, yeah?
    
    - clean up the /tmp directory when it's convenient

--- wsl/ssh-agent.sh
+++ wsl/ssh-agent.sh
@@ -5,16 +5,35 @@
 # wrapper script caches the ssh-agent configuration in a file, so it
 # can be reused if the agent is already running.
 
-SSH_AGENT_INFO="${HOME}/.ssh-agent.sh"
+# Note that, like ssh-agent, this script is expected to be invoked
+# with `eval $(...)`, and therefore it echoes commands for the parent
+# shell to execute.
 
-if [ -e $SSH_AGENT_INFO ]; then
-    source $SSH_AGENT_INFO > /dev/null
+ssh_agent_info="${HOME}/.ssh-agent.sh"
+
+if [ -e $ssh_agent_info ]; then
+
+    source $ssh_agent_info > /dev/null
+
     if ps --pid $SSH_AGENT_PID > /dev/null; then
+
         echo "echo Agent already running;"
-        cat $SSH_AGENT_INFO
+        cat $ssh_agent_info
+
+        # Clean up any stale /tmp/ssh-* directories; we do this here
+        # because we've already loaded the SSH_* variables and know
+        # the agent is running.
+        hot_ssh_tmp=$(dirname $SSH_AUTH_SOCK)
+        for ssh_tmp in /tmp/ssh-*; do
+            if [[ "$ssh_tmp" != "$hot_ssh_tmp" ]]; then
+                echo rm -v -r $ssh_tmp
+            fi
+        done
+
         exit
     fi
 fi
 
 echo "echo Agent started;"
-ssh-agent -s | tee $SSH_AGENT_INFO
+ssh-agent -s | tee $ssh_agent_info
+echo ssh-add
